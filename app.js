1bdda7c74b6cca71f1de030c520c9630e5af47c7
Dayana Mariano <dayanaraquelle@gmail.com>
2023-11-26 20:05:15 -0300

    :sparkles: feat: adicionar função apagarProduto

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const listaCompras = [];
+const elementoLista = document.getElementΒyId("lista"); //ul
+
 document
   .getElementById("formAdicionar")
   .addEventListener("submit", adicionarProduto);
 
 function adicionarProduto(event) {
   event.preventDefault();
-  const elementoLista = document.getElementById("lista"); //ul
-  const elementoProduto = document.getElementById("novoProduto"); //input
 
-  const novoProduto = {
-    nome: elementoProduto.value,
-    status: "pendente",
-  };
-
+  const elementoProduto = document.getElementById("inputAdicionar"); //input
+  const novoProduto = elementoProduto.value;
   listaCompras.push(novoProduto);
 
-  elementoLista.innerHTML += `<li> 
-    ${novoProduto.nome}
+  elementoLista.innerHTML += `<li id="liProduto${novoProduto}"> 
+    <input class="check" type="checkbox" id="status">
+    <input type="text" value="${novoProduto}" id="${novoProduto}">
+    <button id="buttonApagar" onclick="apagarProduto(${novoProduto}, liProduto${novoProduto})"> Apagar </button>
   </li>`;
+
   elementoProduto.value = "";
+}
+
+function apagarProduto(novoProduto) {
+  let produtoApagado = novoProduto.value
+
+  listaCompras.splice(produtoApagado, 1)
+  console.log(listaCompras)
+
+  let nomeDoLi = "liProduto" + novoProduto.value
+  console.log(nomeDoLi)
+
+  let elementoLiAtual = document.getElementById(nomeDoLi)
+  console.log(elementoLiAtual)
+  elementoLista.removeChild(elementoLiAtual)
 }
